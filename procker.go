9e6cb1e93d7307295048338fa8726cd96056b0ab
Jonhnny Weslley <jw@jonhnnyweslley.net>
2014-05-01 01:57:51 -0300

    expand command before starting processes

--- procker.go
+++ procker.go
@@ -2,6 +2,7 @@ package procker
 
 import (
 	"io"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -16,9 +17,10 @@ func NewProcess(name, command string) *Process {
 	return &Process{Name: name, Command: command}
 }
 
-func (p *Process) Start(out, err io.Writer) error {
-	args := strings.Fields(p.Command)
+func (p *Process) Start(env []string, out, err io.Writer) error {
+	args := strings.Fields(p.expandedCmd(env))
 	p.cmd = exec.Command(args[0], args[1:]...)
+	p.cmd.Env = env
 	p.cmd.Stdout = out
 	p.cmd.Stderr = err
 	return p.cmd.Start()
@@ -26,4 +28,22 @@ func (p *Process) Start(out, err io.Writer) error {
 
 func (p *Process) Wait() error {
 	return p.cmd.Wait()
+}
+
+func (p *Process) expandedCmd(env []string) string {
+	m := env2Map(env)
+	return os.Expand(p.Command, func(name string) string {
+		return m[name]
+	})
+}
+
+func env2Map(env []string) map[string]string {
+	m := make(map[string]string)
+	for _, value := range env {
+		pair := strings.SplitN(value, "=", 2)
+		if len(pair) == 2 {
+			m[pair[0]] = pair[1]
+		}
+	}
+	return m
 }
--- procker_test.go
+++ procker_test.go
@@ -8,9 +8,10 @@ import (
 func TestProcessStart(t *testing.T) {
 	stdOut := &bytes.Buffer{}
 	stdErr := &bytes.Buffer{}
+	var env []string
 
 	p := NewProcess("simple", "echo -n procker")
-	err := p.Start(stdOut, stdErr)
+	err := p.Start(env, stdOut, stdErr)
 
 	if err != nil {
 		t.Fatal("process failed")
@@ -22,6 +23,32 @@ func TestProcessStart(t *testing.T) {
 	}
 
 	if stdOut.String() != "procker" {
+		t.Error("bad output")
+	}
+
+	if stdErr.String() != "" {
+		t.Error("bad output")
+	}
+}
+
+func TestProcessStartUsingEnv(t *testing.T) {
+	stdOut := &bytes.Buffer{}
+	stdErr := &bytes.Buffer{}
+	var env []string = []string{"PROCKER_MSG=hello", "PROCKER_MSG2=world"}
+
+	p := NewProcess("simple", "echo -n $PROCKER_MSG $PROCKER_MSG2")
+	err := p.Start(env, stdOut, stdErr)
+
+	if err != nil {
+		t.Fatal("process failed")
+	}
+
+	err = p.Wait()
+	if err != nil {
+		t.Fatal("process failed")
+	}
+
+	if stdOut.String() != "hello world" {
 		t.Error("bad output")
 	}
 
