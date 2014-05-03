21ac1f6e45cc1132751e50509a02c01986097c66
Jonhnny Weslley <jw@jonhnnyweslley.net>
2014-05-03 11:44:24 -0300

    allow run multiple command and kill them on exit

--- procker/procker.go
+++ procker/procker.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"log"
 	"os"
+	"os/signal"
 	"path"
+	"syscall"
 
 	"github.com/jweslley/procker"
 )
@@ -24,10 +26,26 @@ func main() {
 	}
 	log.Println(processes)
 
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		for sig := range c {
+			log.Printf("%v received, stopping processes and exiting.", sig)
+			for name, process := range processes {
+				log.Printf("killing %s", name)
+				process.Kill()
+			}
+			os.Exit(1)
+		}
+	}()
+
 	wd := path.Dir(*procfile)
 	for name, process := range processes {
 		log.Printf("starting %s - %s", name, process.Command)
 		process.Start(wd, []string{}, os.Stdout, os.Stderr)
+	}
+
+	for _, process := range processes {
 		process.Wait()
 	}
 }
