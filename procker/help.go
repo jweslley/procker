24d04cfef3f4c1f17f04a3e63e86463d0ea0f2f1
Jonhnny Weslley <jonhnny@credishop.com.br>
2014-05-07 11:57:42 +0000

    add help command

--- procker/help.go
+++ procker/help.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 var cmdHelp = &command{
 	desc: "Show this help",
@@ -10,5 +13,17 @@ Help shows usage for a command.`,
 	call: help}
 
 func help(args []string) {
-	fmt.Printf("Help %s version %s\n", programName, programVersion)
+	if len(args) == 0 {
+		usage()
+		os.Exit(0)
+	}
+
+	cmdName := args[0]
+	command, ok := commands[cmdName]
+	if !ok {
+		fmt.Fprintf(os.Stderr, "procker: '%s' is not a procker command. See 'procker help'.\n", cmdName)
+		os.Exit(1)
+	}
+
+	fmt.Println(command.help)
 }
